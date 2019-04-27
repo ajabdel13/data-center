7bb5d3b466a7dfa67939a32c31bbebca056feea8
ajabdel13 <48064320+ajabdel13@users.noreply.github.com>
2019-04-27 20:49:05 +0200

    Update google-data-center.py

--- google-data-center.py
+++ google-data-center.py
@@ -35,10 +35,10 @@ class DatacenterEnv(object):
         
         #energy with no ai
          energy_noai = 0
-        if (self.temperature_noai > self.optimal_temperature[1])
+        if (self.temperature_noai > self.optimal_temperature[1]):
             enregy_noai = self.optimal_temperature[1] - self.temperature_noai
             self.temperature_noai = self.optimal_temperature[1]
-        elif (self.temperature_noai < self.optimal_temperature[0])
+        elif (self.temperature_noai < self.optimal_temperature[0]):
             energy_noai = self.temperature_noai - self.optimal_temperature[0]
             self.temperature_ai = self.optimal_temperature[0]
 
@@ -48,22 +48,22 @@ class DatacenterEnv(object):
         self.atmospheric_temperature = self.monthly_atmospheric_temperature[month]
         # current users
         self.current_users += np.random.randint(-self.max_update_users, self.max_update_users)
-        if (self.current_users> self.max_users)
+        if (self.current_users> self.max_users):
            self.current_users = self.max_users
-        elif (self.current_users < self.min_users)
+        elif (self.current_users < self.min_users):
            self.current_users = self.min_users
         # current rate data 
         self.current_rate_data += np.random.randint(-self.max_update_data, self.max_update_data)
-        if (self.current_rate_data > self.max_rate_data)
+        if (self.current_rate_data > self.max_rate_data):
            self.current_rate_data = self.max_rate_data
-        elif (self.current_rate_data < self.min_rate_data)
+        elif (self.current_rate_data < self.min_rate_data):
            self.current_rate_data = self.min_rate_data
 
         past_intrinsic_temperature = self.intrinsic_temperature
         self.intrinsic_temperature = 1.25 * self.atmospheric_temperature + 1.95 * self.current_users + 1.95 * self.current_rate_data
         delta_intrinsic_temperature = self.intrinsic_temperature - past_intrinsic_temperature
 
-        if (direction = -1)
+        if (direction = -1):
            delta_temperature_ai = -energy_ai
         else:
            delta_temperature_ai = energy_ai
@@ -71,17 +71,17 @@ class DatacenterEnv(object):
         self.temperature_ai += delta_temperature_ai + delta_intrinsic_temperature
         self.temperature_noai = delta_intrinsic_temperature
 
-        if (self.temperature_ai > self.max_temperature)
+        if (self.temperature_ai > self.max_temperature):
            if (self.train== 1):
                self.game_over = 1
            else:
                self.total_energy_ai += self.temperature_ai - self.optimal_temperature[1] 
                self.temperature_ai = self.optimal_temperature[1]
 
-        if (self.temeperature_ai < self.min_temperature)
-           if (self.train == 1)
+        if (self.temeperature_ai < self.min_temperature):
+           if (self.train == 1):
               self.game_over = 1
-           else 
+           else :
               self.total_energy_ai += self.optimal_temperature[0] - self.temeperature_ai
               self.temeperature_ai = self.optimal_temperature[0]
 
@@ -94,7 +94,7 @@ class DatacenterEnv(object):
         next_state = np.matrix([scaled_temperature_ai, scaled_current_users,scaled_current_rate_data])
         return next_state, self.reward, self.game_over
     
-    def reset (self, month)
+    def reset (self, month):
         self.atmospheric_temperature = self.monthly_atmospheric_temperature[month]
         self.initial_users = initial_users
         self.current_users = initial_users
@@ -130,7 +130,7 @@ step_max = 1000
 #Create The Brain 
 class Brain:
 
-    def __init__(self, state_size, action_size, learning_rate, Training, name="DQNbrain")
+    def __init__(self, state_size, action_size, learning_rate, Training, name="DQNbrain"):
        self.state_size = state_size
        self.action_size = action_size
        self.learning_rate = learning_rate
@@ -188,15 +188,15 @@ memory = Memory(10000)
 
 #Handle the problem of empty Memory
 DataCenter.reset()
-for i in range (pretrain_length)
+for i in range (pretrain_length):
      
      state = Datacenter.Observe()
      month=np.random.randint(0,12)
      action = np.random.randint(0,5)
-     if (action-action_boundry)< 0
+     if (action-action_boundry)< 0 :
          direction = -1
          energy_ai = abs(action-action_boundry) * 1.5
-     else
+     else:
          direction = 1
          energy_ai = abs(action - action_boundry) * 1.5
      next_state, self.game_over, self.reward = update_env_ai(self, energy_ai, direction, month)
@@ -206,7 +206,7 @@ for i in range (pretrain_length)
           memory.add((state, next_state, self.reward, action, self.game_over))
           DataCenter.reset()
           state= Datacenter.Observe()
-     else 
+     else :
           memory.add((state, next_state, self.reward, action, self.game_over))
           state = next_state
 
@@ -314,13 +314,13 @@ if(training= True):
                            save_path = saver.save(sess,"./models/model.ckpt")
                            print("Model Saved")
 #Test Model:
-with tf.Session() as sess
+with tf.Session() as sess:
 
      DataCenter= Datacenterenv()
      total_score = 0
      saver.restore(sess, "./models/model.ckpt")
 
-     for i in range (90):
+     for timestep in range (10000):
 
          self.game_over = False
          DataCenter.reset()
@@ -332,7 +332,21 @@ with tf.Session() as sess
 
               choice = np.argmax(Qs)
               action = possible_action[int(choice)]
-              action -
+              if ( action - action_boundry< 0):
+                  direction = -1
+                  energy_ai = abs(action - action_boundry)*1.5
+              else:
+                  direction = 1
+                  energy_ai = abs(action - action_boundry)*1.5
+                  
+              next_state, reward, game_over = Datacenter.update_env_ai(energy_ai,  direction, timestep/(30*24*60))
+              state = next_state
+print("\n")
+print("Total Energy spent with an AI: {:.0f}".format(env.total_energy_ai))
+print("Total Energy spent with no AI: {:.0f}".format(env.total_energy_noai))
+print("ENERGY SAVED: {:.0f} %".format((env.total_energy_noai - env.total_energy_ai) / env.total_energy_noai * 100))               
+            
+            
 
 
 
